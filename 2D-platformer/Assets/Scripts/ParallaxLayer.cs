fb4572ee504db8a760ab25fd3de3c046a35131e1
tommyasanders98 <163438057+tommyasanders98@users.noreply.github.com>
2025-04-30 21:11:02 -0600

    Parallax v2

--- 2D-platformer/Assets/Scripts/ParallaxLayer.cs
+++ 2D-platformer/Assets/Scripts/ParallaxLayer.cs
@@ -1,18 +1,46 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ParallaxLayer : MonoBehaviour
 {
-    public float parallaxFactor = 0.5f;
-    private Vector3 startLocalPos;
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
 
-    void Start()
+    private void Start()
     {
-        startLocalPos = transform.localPosition;
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
-        Vector3 camOffset = transform.parent.position;
-        transform.localPosition = startLocalPos + new Vector3(camOffset.x * parallaxFactor, camOffset.y * parallaxFactor, 0);
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y, 0f);
+        lastCameraPosition = cameraTransform.position;
+
+        if (cameraTransform.position.x - transform.position.x >= textureUnitSizeX)
+        {
+            float offsetPositonX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositonX, transform.position.y);
+        }
     }
+
+    //public float parallaxFactor = 0.5f;
+    //private Vector3 startLocalPos;
+
+    //void Start()
+    //{
+    //    startLocalPos = transform.localPosition;
+    //}
+
+    //void LateUpdate()
+    //{
+    //    Vector3 camOffset = transform.parent.position;
+    //    transform.localPosition = startLocalPos + new Vector3(camOffset.x * parallaxFactor, camOffset.y * parallaxFactor, 0);
+    //}
 }
--- 2D-platformer/Assets/Scripts/ParallaxLooper.cs
+++ 2D-platformer/Assets/Scripts/ParallaxLooper.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 
 public class ParallaxLooper : MonoBehaviour
 {
-    public float parallaxFactor = 0.5f;
     public bool loop = true;
-
     private float spriteWidth;
     private Transform cam;
-    private Vector3 lastCamPosition;
 
     void Start()
     {
         cam = Camera.main.transform;
-        lastCamPosition = cam.position;
 
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         spriteWidth = sr.bounds.size.x;
@@ -20,19 +16,15 @@ public class ParallaxLooper : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector3 delta = cam.position - lastCamPosition;
-        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
-        lastCamPosition = cam.position;
+        if (!loop) return;
 
-        if (loop)
+        float camHorizontalExtent = Camera.main.orthographicSize * Screen.width / Screen.height;
+        float distanceFromCamera = cam.position.x - transform.position.x;
+
+        if (Mathf.Abs(distanceFromCamera) >= spriteWidth)
         {
-            float camHorizontalExtent = Camera.main.orthographicSize * Screen.width / Screen.height;
-
-            if (Mathf.Abs(cam.position.x - transform.position.x) >= spriteWidth)
-            {
-                float offset = (cam.position.x - transform.position.x) % spriteWidth;
-                transform.position = new Vector3(cam.position.x + offset, transform.position.y, transform.position.z);
-            }
+            float offset = (distanceFromCamera % spriteWidth);
+            transform.position += new Vector3(spriteWidth * 2 * Mathf.Sign(offset), 0, 0);
         }
     }
 }
