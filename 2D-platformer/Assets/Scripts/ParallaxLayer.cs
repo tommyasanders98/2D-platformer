419703fecd3c74d44c782678502063fd8b830f78
tommyasanders98 <163438057+tommyasanders98@users.noreply.github.com>
2025-05-07 14:35:05 -0600

    update

--- 2D-platformer/Assets/Scripts/ParallaxLayer.cs
+++ 2D-platformer/Assets/Scripts/ParallaxLayer.cs
@@ -5,42 +5,68 @@ public class ParallaxLayer : MonoBehaviour
 {
     [SerializeField] private Vector2 parallaxEffectMultiplier;
     private Transform cameraTransform;
-    private Vector3 lastCameraPosition;
-    private float textureUnitSizeX;
+    private Vector3 startCameraPosition;
+    private Vector3 startLayerPosition;
+    private float pixelsPerUnit;
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        Texture2D texture = sprite.texture;
-        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        startCameraPosition = cameraTransform.position;
+        startLayerPosition = transform.position;
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        pixelsPerUnit = sr.sprite.pixelsPerUnit;
     }
 
     private void LateUpdate()
     {
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y, 0f);
-        lastCameraPosition = cameraTransform.position;
+        // Parallax offset relative to where the camera started
+        Vector3 camDelta = cameraTransform.position - startCameraPosition;
 
-        if (cameraTransform.position.x - transform.position.x >= textureUnitSizeX)
-        {
-            float offsetPositonX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3(cameraTransform.position.x + offsetPositonX, transform.position.y);
-        }
+        Vector3 newPos = startLayerPosition + new Vector3(
+            camDelta.x * parallaxEffectMultiplier.x,
+            camDelta.y * parallaxEffectMultiplier.y,
+            0f
+        );
+
+        // Snap to pixel grid
+        newPos.x = Mathf.Round(newPos.x * pixelsPerUnit) / pixelsPerUnit;
+        newPos.y = Mathf.Round(newPos.y * pixelsPerUnit) / pixelsPerUnit;
+
+        transform.position = newPos;
     }
 
-    //public float parallaxFactor = 0.5f;
-    //private Vector3 startLocalPos;
 
-    //void Start()
+
+
+    //[SerializeField] private Vector2 parallaxEffectMultiplier;
+    //private Transform cameraTransform;
+    //private Vector3 lastCameraPosition;
+    //private float textureUnitSizeX;
+
+    //private void Start()
     //{
-    //    startLocalPos = transform.localPosition;
+    //    cameraTransform = Camera.main.transform;
+    //    lastCameraPosition = cameraTransform.position;
+    //    Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+    //    Texture2D texture = sprite.texture;
+    //    textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     //}
 
-    //void LateUpdate()
+    //private void LateUpdate()
     //{
-    //    Vector3 camOffset = transform.parent.position;
-    //    transform.localPosition = startLocalPos + new Vector3(camOffset.x * parallaxFactor, camOffset.y * parallaxFactor, 0);
+
+
+    //    Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+    //    transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y, 0f);
+    //    lastCameraPosition = cameraTransform.position;
+
+    //    if (cameraTransform.position.x - transform.position.x >= textureUnitSizeX)
+    //    {
+    //        float offsetPositonX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+    //        transform.position = new Vector3(cameraTransform.position.x + offsetPositonX, transform.position.y);
+    //    }
     //}
 }
+
